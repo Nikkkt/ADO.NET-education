0e1b488486d6f021a65b463f19c7e7b616cd8cfa
Nikkkt <work.terpilovsky@gmail.com>
2024-03-31 04:15:28 +0300

    ADO.NET education

---
+++ FruitsAndVegetables/FruitsAndVegetables/AsyncProgram.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+// To start file you need to comment all code in Program.cs
+class Program
+{
+    static async Task Main()
+    {
+        string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["FruitsAndVegetablesDB"].ConnectionString;
+
+        DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
+        DbProviderFactory factory = DbProviderFactories.GetFactory("System.Data.SqlClient");
+
+        using (var connection = factory.CreateConnection())
+        {
+            connection.ConnectionString = CONNECTION_STRING;
+
+            try
+            {
+                await connection.OpenAsync();
+                Console.WriteLine("Connection open\n");
+
+                var command = factory.CreateCommand();
+                command.Connection = connection;
+
+                command.CommandText = "SELECT * FROM Produce";
+                using (var reader = await command.ExecuteReaderAsync()) { while (await reader.ReadAsync()) Console.WriteLine(string.Format("Name: {0,-10}\tType: {1,-10}\tColour: {2,-10}\tCalories: {3,-10}", reader["name"], reader["type"], reader["color"], reader["calories"])); }
+
+                command.CommandText = "UPDATE Produce SET name = 'Apples' WHERE id = 1";
+                await command.ExecuteNonQueryAsync();
+
+                command.CommandText = "DELETE FROM Produce WHERE id = 10";
+                await command.ExecuteNonQueryAsync();
+
+                await connection.CloseAsync();
+            }
+            catch (DbException ex) { Console.WriteLine("Connection error: " + ex.Message); }
+        }
+    }
+}
---
+++ FruitsAndVegetables/FruitsAndVegetables/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["FruitsAndVegetablesDB"].ConnectionString;
+
+Console.Write("Choose DBMS:\n1 - SQL Server\n2 - MySQL\n3 - PostgreSQL\n>>> ");
+var dbChoice = Console.ReadLine();
+
+string providerName = dbChoice switch { "1" => "System.Data.SqlClient", "2" => "MySql.Data.MySqlClient", "3" => "Npgsql", _ => throw new Exception("Невідомий вибір СКБД") };
+
+DbProviderFactories.RegisterFactory(providerName, SqlClientFactory.Instance);
+DbProviderFactory factory = DbProviderFactories.GetFactory(providerName);
+
+using (var connection = factory.CreateConnection())
+{
+    connection.ConnectionString = CONNECTION_STRING;
+
+    try
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        connection.Open();
+        Console.WriteLine("Connection open\n");
+
+        var command = factory.CreateCommand();
+        command.Connection = connection;
+
+        command.CommandText = "SELECT * FROM Produce";
+        using (var reader = command.ExecuteReader()) { while (reader.Read()) Console.WriteLine(string.Format("Name: {0,-10}\tType: {1,-10}\tColour: {2,-10}\tCalories: {3,-10}", reader["name"], reader["type"], reader["color"], reader["calories"])); }
+        Console.WriteLine($"\nTime: {stopwatch.Elapsed.TotalSeconds} s\n");
+
+        command.CommandText = "SELECT Name FROM Produce";
+        using (var reader = command.ExecuteReader()) { while (reader.Read()) Console.WriteLine($"Name: {reader["name"]}"); }
+        Console.WriteLine($"\nTime: {stopwatch.Elapsed.TotalSeconds} s\n");
+
+        command.CommandText = "SELECT DISTINCT color FROM Produce";
+        using (var reader = command.ExecuteReader()) { while (reader.Read()) Console.WriteLine($"Colour: {reader["color"]}"); }
+        Console.WriteLine($"\nTime: {stopwatch.Elapsed.TotalSeconds} s\n");
+
+        command.CommandText = "SELECT MAX(calories) FROM Produce";
+        Console.WriteLine($"Max calories: {command.ExecuteScalar()}");
+        Console.WriteLine($"\nTime: {stopwatch.Elapsed.TotalSeconds} s\n");
+
+        command.CommandText = "SELECT MIN(calories) FROM Produce";
+        Console.WriteLine($"Min calories: {command.ExecuteScalar()}");
+        Console.WriteLine($"\nTime: {stopwatch.Elapsed.TotalSeconds} s\n");
+
+        command.CommandText = "SELECT AVG(calories) FROM Produce";
+        Console.WriteLine($"Average calories: {command.ExecuteScalar()}");
+        Console.WriteLine($"\nTime: {stopwatch.Elapsed.TotalSeconds} s\n");
+
+        connection.Close();
+    }
+    catch (DbException ex) { Console.WriteLine("Connection error: " + ex.Message); }
+}
